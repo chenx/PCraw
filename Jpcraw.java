f6586435525e048a176ef84c3f1e9d124d24fd24
Xin Chen <chenx@Thomas.local>
2014-07-27 02:08:45 -1000

    udpate Jpcraw on comments

--- Jpcraw.java
+++ Jpcraw.java
@@ -1,22 +1,32 @@
+//
+// This java program runs PCraw, captures its stdout and stderr output 
+// and display to stdout in real time.
+//
+// This wrapper can be extended to a platform independent GUI for PCraw.
+//
+// To run, type: java Jpcraw -v
+// Parameters are the same as for pcraw.pl
+//
+// Modified from: http://www.perlmonks.org/?node_id=880769
+//
+// @by X. Chen
+// @since 7/25/2014
+//
+
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Arrays;
 
-//
-// This java program runs PCraw, captures stdin output and display.
-// This wrapper can be extended to create a platform independent GUI
-// For PCraw.
-//
-// To run, type: java Jpcraw -v
-// Parameters are the same as for pcraw.pl
-//
-// Modified from: http://www.perlmonks.org/?node_id=880769
-// 
-// @by: X. Chen
-// @since: 7/25/2014
-//
+
+/**
+ * Java wrapper for pcraw.pl.
+ *
+ * @by X. Chen
+ * @since 7/25/2014
+ */
 class Jpcraw {
 
     public static void main(String[] args) throws IOException {
@@ -25,7 +35,7 @@ class Jpcraw {
 
         String[] aCmdArgs = new String[2 + args.length];
         aCmdArgs[0] = "perl";
-        aCmdArgs[1] = "PCraw.pl";
+        aCmdArgs[1] = "pcraw.pl";
         for (int i = 0; i < args.length; ++ i) {
             aCmdArgs[i+2] = args[i];
         }
@@ -35,8 +45,8 @@ class Jpcraw {
 
         try {
             oProcess = oRuntime.exec(aCmdArgs);
-            // This will block here, and won't be able to output in real time.
-            // so remove this, then it'll output in real time.
+            // This will block, and won't be able to output in real time.
+            // Remove this, so it can output in real time.
             //oProcess.waitFor();
         } catch (Exception e) {
             System.out.println("error executing " + aCmdArgs[0]);
